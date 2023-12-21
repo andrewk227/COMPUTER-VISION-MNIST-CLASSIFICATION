11a747ce0fc7aa35eb3d0e304f492b5870bdfbfc
Andrew Ashraf <andrewashraf698@gmail.com>
2023-12-21 18:51:55 +0200

    KNN-classifier

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-import tensorflow
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+from PIL import Image
 
 data = pd.read_csv('mnist_train.csv')
 
@@ -12,13 +14,20 @@ data = data.dropna()
 # 784 (28x28)feature 1 target
 
 features = data.drop(columns=['label'], axis =1)
-print(features)
+#print(features)
 
 features = features/255  # Normalization
 targets = data['label']
 
-# reshaping the image before the split into 28 x 28
-# train_images = train_images.reshape((train_images.shape[0], 28, 28, 1))
+# Reshape images to (28, 28, 1) for compatibility with convolutional layers
+# train_images = features.reshape((features.shape[0], 28, 28, 1))
 
+
+#splitin the data into train an validation sets
 x_train , x_validation , y_train , y_validation = train_test_split(features, targets, test_size=0.2, random_state=42)
 
+KNNclassifier = KNeighborsClassifier(n_neighbors=4)
+KNNclassifier.fit(x_train, y_train)
+y_pred = KNNclassifier.predict(y_train)
+
+print(accuracy_score(y_train, y_pred))
