b560d72f8b5fd3f87ccbf6ffc8e92a12477f9467
Andrew Ashraf <andrewashraf698@gmail.com>
2023-12-22 02:24:06 +0200

    KNN using grid search technique

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import pandas as pd
+import numpy as np
+from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 from PIL import Image
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
 
 data = pd.read_csv('mnist_train.csv')
-
-print(data.describe())
+# print(data.describe())
 
 data = data.dropna()
 
@@ -16,18 +20,45 @@ data = data.dropna()
 features = data.drop(columns=['label'], axis =1)
 #print(features)
 
-features = features/255  # Normalization
+# Normalization the data by dividing it by 255
+features = features/255  
 targets = data['label']
 
-# Reshape images to (28, 28, 1) for compatibility with convolutional layers
-# train_images = features.reshape((features.shape[0], 28, 28, 1))
+# Resize images to dimensions of 28 by 28
+images = []
+for row in features.values:
+    temp = row.reshape(28, 28).tolist()
+    images.append(temp)
+    
 
+# Visualize some images to verify the correctness of the reshaping process
+num_images_to_visualize = 5
+plt.figure(figsize=(10, 4))
+for i in range(num_images_to_visualize):
+    plt.subplot(2, num_images_to_visualize, i + 1)
+    plt.imshow(images[i], cmap='gray')
+    plt.title(f'resized {i + 1}')
+    plt.axis('off')
 
-#splitin the data into train an validation sets
+plt.tight_layout()
+plt.show()
+
+#spliting the data into train and validation sets
 x_train , x_validation , y_train , y_validation = train_test_split(features, targets, test_size=0.2, random_state=42)
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Intial Experiment ----> KNN using grid search technique
+KNN = KNeighborsClassifier()
 
-KNNclassifier = KNeighborsClassifier(n_neighbors=4)
-KNNclassifier.fit(x_train, y_train)
-y_pred = KNNclassifier.predict(y_train)
+# Create the parameter grid based on the results of random search
+grid = {'n_neighbors': [2,4,6,8] }
+GridSearch = GridSearchCV(KNN, grid, cv=2, scoring ='accuracy')
+GridSearch.fit(x_train, y_train)
+print ('Best Param', GridSearch.best_params_)
 
-print(accuracy_score(y_train, y_pred))
+# Get the best model
+best_knn_model = GridSearch.best_estimator_
+
+# Evaluate the best model on the test set
+accuracy = best_knn_model.score(x_train, y_train)
+print("Test Accuracy:", accuracy)
+#-----------------------------------------------------------------------------------------------------------------------------------------------
